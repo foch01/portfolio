06e12f9d921970e51a9a3c9c6a856773efb34958
foch01 <tsanceo@yahoo.com>
2017-01-12 17:31:16 +0100

    start project

---
+++ portfolio-1/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <title>Thomas Sanceo</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+
+        <div class="left">
+            <div class="profil"></div>
+            <div class="name">Thomas Sanceo</div>
+            <div class="describe">Développeur Web, passionné par le sport, les technologies et le webmarketing</div>
+            <div class="social">
+            <div class="social-1">
+            <a href="#" class=" facebook ico-perso">
+                <i class="fa fa-facebook-official fa-4x" aria-hidden="true"></i>
+            </a>
+            <a href="#" class="twitter ico-perso">
+                <i class="fa fa-twitter-square fa-4x" aria-hidden="true"></i>
+            </a>
+            <a href="#" class="linkedin ico-perso">
+                <i class="fa fa-linkedin-square fa-4x" aria-hidden="true"></i>
+            </a>
+            </div>
+            <div class="social-2">
+            <a href="#" class="viadeo ico-perso">
+                <i class="fa fa-viadeo-square fa-4x" aria-hidden="true"></i>
+            </a>
+            <a href="#" class="github ico-perso">
+                <i class="fa fa-github-square fa-4x" aria-hidden="true"></i>
+            </a>
+            <a href="#" class="cv ico-perso">
+                <i class="fa fa-file fa-4x" aria-hidden="true"></i>
+            </a>
+            </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="portfolio">
+
+                <a href="">
+                    <img src="images/createpro.png" alt="" class="img">
+                </a>
+
+                <a href="">
+                    <img src="images/createpro.png" alt="" class="img">
+                </a>
+                <a href="">
+                     <img src="images/createpro.png" alt="" class="img">
+                </a>
+            </div>
+            <div class="portfolio">
+                <a href="">
+                    <img src="images/createpro.png" alt="" class="img">
+                </a>
+                <a href="">
+                    <img src="images/createpro.png" alt="" class="img">
+                </a>
+                <a href="">
+                    <img src="images/createpro.png" alt="" class="img">
+                </a>
+            </div>
+            <div class="portfolio">
+                <img src="images/createpro.png" alt="" class="img">
+                <img src="images/createpro.png" alt="" class="img">
+                <img src="images/createpro.png" alt="" class="img">
+            </div>
+
+        </div>
+        
+    </div>
+</body>
+
+</html>
